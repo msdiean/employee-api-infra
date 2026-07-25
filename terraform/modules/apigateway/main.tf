73970d8f79aca9630d5c100cf2e3482e8cce2426
msdiean <168319996+msdiean@users.noreply.github.com>
2026-07-25 10:33:41 +0530

    Update main.tfvaldated

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -198,7 +198,14 @@ resource "aws_api_gateway_stage" "this" {
   cache_cluster_size    = "0.5"
   client_certificate_id = aws_api_gateway_client_certificate.this.id
 
-  method_settings {
+}
+
+resource "aws_api_gateway_method_settings" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  stage_name  = aws_api_gateway_stage.this.stage_name
+  method_path = "*/*"
+
+  settings {
     metrics_enabled        = true
     logging_level          = "INFO"
     data_trace_enabled     = true
