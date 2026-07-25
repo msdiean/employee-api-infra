b6bfb7e74427344de8af78d9e2c34248f4a8ae7b
Karthik M <karthikmsd797.com>
2026-07-25 13:15:26 +0530

    Fix WAF logging ARN (remove :*) and Lambda ZIP path (use working directory relative path)

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -334,7 +334,7 @@ resource "aws_wafv2_web_acl" "this" {
 
 resource "aws_wafv2_web_acl_logging_configuration" "this" {
   resource_arn            = aws_wafv2_web_acl.this.arn
-  log_destination_configs = ["${aws_cloudwatch_log_group.access_logs.arn}:*"]
+  log_destination_configs = [aws_cloudwatch_log_group.access_logs.arn]
 
   logging_filter {
     default_behavior = "KEEP"
