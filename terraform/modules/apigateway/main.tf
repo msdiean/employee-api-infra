bf48dd2d685fc0b33e55914b9b69ce0aa3f98c8b
Karthik M <karthikmsd797.com>
2026-07-25 13:48:03 +0530

    Fix WAF logging destination ARN format: add :* suffix as required by AWS WAFv2

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -352,7 +352,7 @@ resource "aws_wafv2_web_acl" "this" {
 
 resource "aws_wafv2_web_acl_logging_configuration" "this" {
   resource_arn            = aws_wafv2_web_acl.this.arn
-  log_destination_configs = [aws_cloudwatch_log_group.access_logs.arn]
+  log_destination_configs = ["${aws_cloudwatch_log_group.access_logs.arn}:*"]
 
   depends_on = [
     aws_cloudwatch_log_group.access_logs,
